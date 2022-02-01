9a59a2155bca1d1ffa6a71b094169a63c0806195
vitorchx@outlook.com <vitorchx@outlook.com>
2022-02-01 18:54:18 -0300

    att

--- controle/doWhile.js
+++ controle/doWhile.js
@@ -6,8 +6,8 @@ function getInteiroAleatorioEntre(min, max) {
 let opcao = -1
 
 do {
-opcao = getInteiroAleatorioEntre(-1, 10)
-console.log('Opção escolhida foi ' + opcao + '.')
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log('Opção escolhida foi ' + opcao + '.')
 
 } while (opcao != -1)
 
